8cfed1cca18c5f0a71f631412d97c7e6588bcc4a
Juan <franciscoalfaro2008@gmail.com>
2017-04-23 21:30:29 -0400

    Finish encrypt

--- userland/testbin/encrypt/encrypt.c
+++ userland/testbin/encrypt/encrypt.c
@@ -11,9 +11,8 @@
 static
 int encrypt_test(const char *file, int encr_times, char * writebuf, const int size) {
     int fd, rv; 
-    //char* readbuf = (char*)malloc(sizeof(char) * size);
+
 	static char readbuf[41];
-	//static char readbuf2[BUF2];
 
     fd = open(file, O_RDWR|O_CREAT|O_TRUNC, 0664);
  
@@ -42,20 +41,37 @@ int encrypt_test(const char *file, int encr_times, char * writebuf, const int si
  
         rv = encrypt(fd);
 
-	//if i ==0 print out last 16 bytes
-	//int x;  	
-	//for(x=BUFF1-16;x<BUFF1;x++);
-	if(i==0)
-		printf ("last 16 bytes after first encrypt:\n%s\n", &(writebuf[strlen (writebuf) - 16]));
-
-        rv = close(fd);
-        if (rv<0) {
+	rv = close(fd);
+	
+	if (rv<0) {
             err(1, "%s: close (1st time)", file);
         }
+
+	//if i ==0 print out last 16 bytes
+	if(i==0){
+		fd = open(file, O_RDWR, 0664);
+ 
+        	if (fd<0) {
+            		err(1, "%s: open for read", file);
+        	}
+
+		//then read from file
+		rv = read(fd, readbuf, 41);
+		if (rv<0) {
+			err(1, "%s: read", file);
+		}
+
+		//then close
+		printf ("\nlast 16 bytes after encrypt:\n%s\n", &(readbuf[strlen (writebuf) - 16]));
+		
+		rv = close(fd);
+        	if (rv<0) {
+            		err(1, "%s: close (1st time)", file);
+        	}
+	
+	}
     }
     
-    //when we break from for loop print last 16 bytes
-    printf ("last 16 bytes after encrypt:\n%s\n", &(writebuf[strlen (writebuf) - 16]));
 
     //check if old print is equivalent to new
 	//open file again read its contents and compare them
@@ -73,12 +89,12 @@ int encrypt_test(const char *file, int encr_times, char * writebuf, const int si
 
 	//then close
 	rv = close(fd);
-	printf("Closing fd=%d retval=%d.\n", fd, rv);
+	//printf("Closing fd=%d retval=%d.\n", fd, rv);
+
 	if (rv<0) {
 		err(1, "%s: close (2nd time)", file);
 	}
 	//to check if they are the same as before
-	printf ("\nlast 16 bytes after read, readbuf contents:\n%s\n", &(readbuf[strlen (readbuf) - 16]));
 
 	if (strcmp(&(readbuf[strlen (readbuf) - 16]), &(writebuf[strlen (writebuf) - 16]))!=0) {
 		err(1, "Buffer data mismatch!");
@@ -92,21 +108,12 @@ int encrypt_test(const char *file, int encr_times, char * writebuf, const int si
 int
 main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
+    (void)argc;
+    (void)argv;
    
     static char writebuf1[BUF1] = "Twiddle dee dee, Twiddle dum dum.......\n";
-    static char writebuf2[BUF2] = "Twiddle dee dee, Twiddle dum dum.......zf\n";
-    //static char writebuf3[BUF3] = "Twiddl\n";
-   
-    //encrypt_test("testfilea1", 1, writebuf3, BUF3);
 
-    encrypt_test("testfilea1", 1, writebuf1, BUF1);
     encrypt_test("testfilea2", 16, writebuf1, BUF1);
-    encrypt_test("testfilea3", 17, writebuf1, BUF1);
-    encrypt_test("testfileb1", 1, writebuf2, BUF2);
-    encrypt_test("testfileb2", 16, writebuf2, BUF2);
-    encrypt_test("testfileb3", 17, writebuf2, BUF2);
   
     return 0;  
 }
